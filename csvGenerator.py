551cab7aaa377a0bf70a120531a2d80b6ceb407c
coffeeologist <liuamy000@gmail.com>
2019-09-13 09:31:37 -0400

    Fixed csvGenerator to work on a directory of folders; added test.wavs for testing

--- csvGenerator.py
+++ csvGenerator.py
@@ -2,15 +2,19 @@
 
 import os
 import subprocess
+import sys
 
-wavFileSourceDirectory="/Users/nehachintamaneni/Documents/Eberly_Center_Data/wavFiles"
+def csvGenerator(path):
+	if (os.path.isdir(path) == False):
+		if path.endswith(".wav"):
+			stringCommand="ffprobe -f lavfi -i amovie="+path+",astats=metadata=1:reset=1 -show_entries frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.Min_difference,lavfi.astats.Overall.Max_difference,lavfi.astats.Overall.Peak_level,lavfi.astats.Overall.RMS_peak,lavfi.astats.Overall.RMS_trough,lavfi.astats.Overall.Crest_factor,lavfi.astats.Overall.Flat_factor,lavfi.astats.Overall.Peak_count, -of csv=p=0 -print_format csv>"+path[:-4]+".csv"
+			subprocess.call(stringCommand, shell=True)
+	else:
+		for filename in os.listdir(path):
+			csvGenerator(path + "/" + filename)
 
+csvGenerator(sys.argv[1])
 
-for file in os.listdir(wavFileSourceDirectory):
-	stringCommand="ffprobe -f lavfi -i amovie="+wavFileSourceDirectory+file+",astats=metadata=1:reset=1 -show_entries frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.RMS_level,lavfi.astats.Overall.DC_offset,lavfi.astats.Overall.Min_level,lavfi.astats.Overall.Max_level,lavfi.astats.Overall.Min_difference,lavfi.astats.Overall.Max_difference,lavfi.astats.Overall.Peak_level,lavfi.astats.Overall.RMS_peak,lavfi.astats.Overall.RMS_trough,lavfi.astats.Overall.Crest_factor,lavfi.astats.Overall.Flat_factor,lavfi.astats.Overall.Peak_count, -of csv=p=0 -print_format csv>"+wavFileSourceDirectory+file[:-4]+".csv"
-	subprocess.call(stringCommand, shell=True)
-
-
-
+# wavFileSourceDirectory="/Users/nehachintamaneni/Documents/Eberly_Center_Data/wavFiles"
 
 
