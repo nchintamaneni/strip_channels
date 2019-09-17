dd75a879d2224bd42a140ebe9f923d51923e9f52
nchintamaneni <53625264+nchintamaneni@users.noreply.github.com>
2019-09-17 15:49:15 -0400

    Add files via upload

--- csvLabeller.py
+++ csvLabeller.py
@@ -11,9 +11,11 @@ def csvLabeller(path):
 			df = pd.read_csv(path, header=None)
 			df.rename(columns={0: 'frame', 1: 'time', 2: 'Peak Level', 3: 'RMS Peak', 4: 'Flat Factor', 5: 'Peak Count', 6: 'Dynamic Range'
 }, inplace=True)
+			for i in range(len(df)):
+				df.at[i , "frame"]= str(i)
 			df.to_csv(path[:-4]+"updated.csv", index=False)
 						
 	else:
 		for filename in os.listdir(path):
 			csvLabeller(path + "/" + filename)
-csvLabeller("/Users/nehachintamaneni/output.csv")
+csvLabeller("/Users/nehachintamaneni/Downloads/2019-02-19-TRec3Combined_decreasedSampleRate.csv")
