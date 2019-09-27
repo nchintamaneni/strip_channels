7f9165309846b6d29667bd63a63b5a3332bb2b44
coffeeologist <liuamy000@gmail.com>
2019-09-27 15:42:25 -0400

    Update csvGenerator mistake

--- csvGenerator.py
+++ csvGenerator.py
@@ -8,12 +8,13 @@ import pandas as pd
 def csvGenerator(path):
 	if (os.path.isdir(path) == False):
 		if path.endswith(".wav"):
-			stringCommand="ffprobe -f lavfi -i amovie="+path+",astats=metadata=1:reset=1 -show_entries frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.Peak_level,lavfi.astats.Overall.RMS_peak,lavfi.astats.Overall.Flat_factor,lavfi.astats.Overall.Peak_count,lavfi.astats.Overall.Dynamic_range,lavfi.astats.Overall.RMS_level-of csv=p=0 -print_format csv>"+path[:-4]+".csv"
+			stringCommand="ffprobe -f lavfi -i amovie="+path+",astats=metadata=1:reset=1 -show_entries frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.Peak_level,lavfi.astats.Overall.RMS_peak,lavfi.astats.Overall.Flat_factor,lavfi.astats.Overall.Peak_count,lavfi.astats.Overall.Dynamic_range, -of csv=p=0 -print_format csv>"+path[:-4]+".csv"
+
 			subprocess.call(stringCommand, shell=True)
 
 			# Label the csv (from csvLabeller.py)
 			df = pd.read_csv(path[:-4]+".csv", header=None)
-			df.rename(columns={0: 'frame', 1: 'time', 2: 'Peak Level', 3: 'RMS Peak', 4: 'Flat Factor', 5: 'Peak Count', 6: 'Dynamic Range', 7:'RMS Level'}, inplace=True)
+			df.rename(columns={0: 'frame', 1: 'time', 2: 'Peak Level', 3: 'RMS Peak', 4: 'Flat Factor', 5: 'Peak Count', 6: 'Dynamic Range'}, inplace=True)
 			for i in range(len(df)):
 				df.at[i , "frame"]= str(i)
 			df.to_csv(path[:-4]+".csv", index=False)
